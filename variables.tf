255e345e99d2bd6b7e1ddb41503c24a3613d9fec
Nicolas Vibert <nicolasvibert@nvibert-a01.vmware.com>
2020-09-21 14:28:34 +0100

    Updated variables

--- variables.tf
+++ variables.tf
@@ -8,10 +8,9 @@ variable "vsphere_password" {}
 variable "vsphere_server" {}
 
 
-variable "Subnet12_name"      {}
-variable "Subnet13_name"      {}
-variable "subnet12"           {}
-variable "subnet13"           {}
+variable "Subnet12_name"      { default = "segment12"}
+variable "Subnet13_name"      { default = "segment13" }
+variable "subnet12"           { default = "12.12.12.0/24"}
+variable "subnet13"           { default = "13.13.13.0/24"}
 
 variable "demo_count"         { default = 3 }
-
