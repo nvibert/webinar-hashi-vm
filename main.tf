96328f6319c29a4e5f56bb6faa6b8112c7c5d34c
Nicolas Vibert <nicolasvibert@nvibert-a01.vmware.com>
2020-09-21 14:31:34 +0100

    corrected var

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 provider "vsphere" {
-  vsphere_user                 = var.vsphere_user
-  vsphere_password             = var.vsphere_password
+  user                 = var.vsphere_user
+  password             = var.vsphere_password
   vsphere_server       = var.vsphere_server
   allow_unverified_ssl = true
 }
